c303cbc6bbd81d61dec7d60fc43dd3b13a0bb4eb
Daniel LG <capelanus@gmail.com>
2016-09-02 18:49:36 -0500

    Initial Commit

---
+++ Raymi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Raymi
+//
+//  Created by Daniel López on 2/09/16.
+//  Copyright © 2016 Daniel López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
